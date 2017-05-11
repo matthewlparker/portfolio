b55d96f3596978f53db3b3e41771a4e3c9c15bfc
Matthew Parker <cadburylion@outlook.com>
2017-05-11 14:47:15 -0700

    refactored code to use handlebars template compiler, not working

--- app.js
+++ app.js
@@ -1,28 +1,69 @@
 'use strict';
 
-function Project(title, src, image, description){
-  this.title = title,
-  this.src = src;
-  this.image = image,
-  this.description = description;
-}
+// var neighborhoods = [];
+//
+// // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
+// function Neighborhood (rawDataObject) {
+//   for (key in rawDataObject) {
+//     this[key] = rawDataObject[key];
+//   }
+// };
+
 
 var projects = [];
 
-Project.prototype.toHTML = function(){
-  $('.project-title').html(this.title);
-  $('.project-src').attr('href', this.src);
-  $('.project-image').attr('src', this.image);
-  $('.project-description').html(this.description);
+function Project(rawDataObject) {
+  for (key in rawDataObject) {
+    this[key] = rawDataObject[key];
+  }
 }
 
-$(document).ready(function(){
-  $('section').hide();
-  $('#projects-page').show();
-  new Project('Guardians of the Goats','https://cadburylion.github.io/', 'images/GoG.png', 'goats in need deserve a guardian indeed').toHTML()
-})
+
+Project.prototype.toHtml = function() {
+  // REVIEW: This method on each instance of Neighborhood allows that object to create its own HTML
+  // TODO: Complete this using Handlebars!!!
+  // 1. Get the template from the HTML document
+  var template = $('#project-template').html();
+  // 2. Use Handlebars to "compile" the HTML
+  var templateRender = Handlebars.compile(template);
+  // 3. Do not forget to return the HTML from this method
+  return templateRender(this);
+};
+
+projectDataSets.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(myNewProjectObject){
+  $('#projects').append(myNewProjectObject.toHtml());
+});
+
+
+
+
+
+
+
+
+
+
+//
+// Project.prototype.toHTML = function(){
+//   $('.project-title').html(this.title);
+//   $('.project-src').attr('href', this.src);
+//   $('.project-image').attr('src', this.image);
+//   $('.project-description').html(this.description);
+// }
+
+// $(document).ready(function(){
+//   $('section').hide();
+//   $('#projects').show();
+// //   new Project('Guardians of the Goats','https://cadburylion.github.io/', 'images/GoG.png', 'goats in need deserve a guardian indeed').toHTML()
+// // })
+// });
 
 $('.nav-ul li').on('click', function(){
+  console.log('click');
   $('section').hide();
   if ($(this).hasClass('nav-about')) {
     $('#about-me-page').fadeIn(350);
---
+++ appData.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var projectDataSets = [
+  {
+    title: 'Guardians of the Goats',
+    image: 'images/GoG.png',
+    descrption: 'goats in need deserve a guardian indeed',
+    src: 'https://cadburylion.github.io/',
+  }
+]
