61615a4019462e6ec2b1f304f0b7d356eb55f3ef
Matthew Parker <cadburylion@outlook.com>
2017-05-13 11:56:36 -0700

    refactored code to use clone method to dynamically push project info to DOM

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 'use strict';
 
-function Project(title, src, image, description){
-  this.title = title,
-  this.src = src;
-  this.image = image,
-  this.description = description;
-}
-
 var projects = [];
 
+function Project(projectData){
+  this.title = projectData.title,
+  this.src = projectData.src;
+  this.image = projectData.image,
+  this.description = projectData.description;
+}
+
 Project.prototype.toHTML = function(){
-  $('.project-title').html(this.title);
-  $('.project-src').attr('href', this.src);
-  $('.project-image').attr('src', this.image);
-  $('.project-description').html(this.description);
+  var $newProject = $('article.template').clone();
+
+  $newProject.find('.project-title').html(this.title);
+  $newProject.find('.project-src').attr('href', this.src);
+  $newProject.find('.project-image').attr('src', this.image);
+  $newProject.find('.project-description').html(this.description);
+
+
+  return $newProject;
+
 }
 
 $(document).ready(function(){
   $('section').hide();
   $('#projects-page').show();
-  new Project('Guardians of the Goats','https://cadburylion.github.io/', 'images/GoG.png', 'goats in need deserve a guardian indeed').toHTML()
 })
+
+projectDataSets.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(project){
+  $('#projects-page').append(project.toHTML());
+});
+
 
 $('.nav-ul li').on('click', function(){
   $('section').hide();
