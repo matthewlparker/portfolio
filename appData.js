43434520f4ddd7a0cab480f6ba37e588b8ce296e
Matthew Parker <cadburylion@outlook.com>
2017-05-15 16:00:47 -0700

    Changed code based on Justins observations and left comments

--- appData.js
+++ appData.js
@@ -7,4 +7,16 @@ var projectDataSets = [
     description: 'goats in need deserve a guardian indeed',
     src: 'https://cadburylion.github.io/',
   },
+  {
+    title: 'Guardians of the Goats',
+    image: 'images/GoG.png',
+    description: 'goats in need deserve a guardian indeed',
+    src: 'https://cadburylion.github.io/',
+  },
+  {
+    title: 'Guardians of the Goats',
+    image: 'images/GoG.png',
+    description: 'goats in need deserve a guardian indeed',
+    src: 'https://cadburylion.github.io/',
+  },
 ]
