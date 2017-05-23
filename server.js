1ac5959e4873a3ef4a5c3c941f0949ba0545de7b
Matthew Parker <cadburylion@outlook.com>
2017-05-22 18:49:23 -0700

    Organized my files to a MVC format

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 // const conString = 'postgres://postgres:Sidirume&7@localhost:5432';
 
-app.use(express.static('.'))
+app.use(express.static('./public'))
 
 app.listen(PORT, function() {
 
