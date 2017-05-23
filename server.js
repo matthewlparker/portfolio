5d263a396a323b72346f5bd8fa016ea30769190c
Matthew Parker <cadburylion@outlook.com>
2017-05-23 15:28:17 -0700

    Added a catch-all in server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(express.static('./public'))
 
+// app.get('/about', (request, response) =>
+// response.sendFile('index.html', {root: './public'}));
+
+app.get('*', (request, response) =>
+  response.sendFile('index.html', {root: './public'}));
+
 app.listen(PORT, function() {
 
   console.log('My Portfolio is open on port: ' + PORT);
